bcf2599a559f20f99f6dc53614dec1c24ff3ca6b
ming-der wang <mingderwang@me.com>
2025-03-06 19:02:46 +0800

    upgrade to astro 5.0

--- src/pages/[...slugs].ts
+++ src/pages/[...slugs].ts
@@ -1,9 +1,9 @@
-
 // pages/[...slugs].ts
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 const app = new Elysia()
   .use(swagger())
+  .get("ping", () => 'pong')
 
 export type App = typeof app;
 
@@ -11,3 +11,4 @@ const handle = ({ request }: { request: Request }) => app.handle(request);
 
 export const GET = handle;
 export const POST = handle;
+
